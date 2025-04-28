878bcf88fddd7fef72c8bd31a64b040d65f82c3f
Manikandan R <ManikandanRamasamy@Manikandans-MacBook-Air.local>
2025-04-28 17:49:04 +0530

    Updated code

--- database/db.sql
+++ database/db.sql
@@ -3,7 +3,7 @@ CREATE DATABASE IF NOT EXISTS linksdb;
 USE linksdb;
 
 -- TABLE USER
--- all pasword wil be encrypted using SHA2
+-- All passwords will be encrypted using SHA2
 CREATE TABLE users (
   id INT(11) NOT NULL AUTO_INCREMENT,
   fullname VARCHAR(100) NOT NULL,
@@ -29,7 +29,7 @@ CREATE TABLE links (
   url VARCHAR(255) NOT NULL,
   description TEXT,
   user_id INT(11),
-  created_at timestamp NOT NULL DEFAULT current_timestamp,
+  created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   CONSTRAINT fk_user FOREIGN KEY(user_id) REFERENCES users(id)
 );
 
@@ -40,3 +40,12 @@ ALTER TABLE links
   MODIFY id INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 2;
 
 DESCRIBE links;
+
+-- SESSIONS TABLE
+-- Storing session information
+CREATE TABLE IF NOT EXISTS sessions (
+  session_id VARCHAR(255) NOT NULL PRIMARY KEY,
+  expires INT NOT NULL,                -- Expiry time in Unix timestamp format
+  data JSON,                           -- Storing session data as JSON
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP  -- Time when session was created
+);
