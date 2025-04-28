6fe360e288282120fd8251789b4b5ee62e257bf2
Manikandan R <ManikandanRamasamy@Manikandans-MacBook-Air.local>
2025-04-28 10:54:05 +0530

    Updated code

--- terraform/main.tf
+++ terraform/main.tf
@@ -126,6 +126,7 @@ module "ecs" {
     linksweb-service = {
       cpu    = 4096
       memory = 8192
+      assign_public_ip = true
 
       container_definitions = {
         webapp = {
@@ -269,4 +270,33 @@ resource "aws_ecr_repository" "repos" {
   tags = {
     Name = each.value
   }
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name = "${var.project_name}-igw"
+  }
+}
+
+# Route Table
+resource "aws_route_table" "this" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.this.id
+  }
+
+  tags = {
+    Name = "${var.project_name}-route-table"
+  }
+}
+
+# Route Table Association
+resource "aws_route_table_association" "this" {
+  subnet_id      = aws_subnet.this.id
+  route_table_id = aws_route_table.this.id
 }
